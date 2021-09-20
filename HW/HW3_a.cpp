853d251a9ef2c245f1cb1ef86b9f1258fd9bf9a4
jHoon0223 <aleejaehun@naver.com>
2021-09-20 17:02:28 +0900

    210920 : HW3 1.(a)

--- HW/HW3_a.cpp
+++ HW/HW3_a.cpp
@@ -4,42 +4,50 @@
 
 using namespace std;
 
-double arr[N], mergeSort[N];
+int arr_SEL[N] = {12,30,21,55,25,72,45,50};
+int arr_BUB[N] = {12,30,21,55,25,72,45,50};
+int arr_INS[N] = {12,30,21,55,25,72,45,50};
+int arr_MER[N] = {12,30,21,55,25,72,45,50};
+int arr_QUI[N] = {12,30,21,55,25,72,45,50};
+int arr_HEA[N] = {12,30,21,55,25,72,45,50};
+int mergeSort[N];
 
-void PRINT(double arr[]) {
+void PRINT(int arr[]) {
     for (int i = 0; i < N; i++)
-        printf("%.3f ", arr[i]);
+        printf("%d ", arr[i]);
+    printf("\n");
 }
-void swap(double arr[], int a, int b) {
-    double tmp;
+void swap(int arr[], int a, int b) {
+    int tmp;
 
     tmp = arr[a];
     arr[a] = arr[b];
     arr[b] = tmp;
 }
 
-void SelectionSort(double arr[]) {
+void SelectionSort(int arr[]) {
     for (int i = N-1; i > 0; i--) {
         int maxIndex = i;
         for (int j = 0; j < i; j++)
             maxIndex = arr[maxIndex] > arr[j] ? maxIndex : j;
-        double tmp = arr[maxIndex];
+        int tmp = arr[maxIndex];
         arr[maxIndex] = arr[i];
         arr[i] = tmp;
-    }
-    PRINT(arr);
+
+        PRINT(arr);
+    }    
 }
-void BubbleSort(double arr[]) {
-    for (int i = 0; i < N-1; i++) {
-        for (int j = 1; j < N-1; j++) {
+void BubbleSort(int arr[]) {
+    for (int i = 0; i < N-1; i++) {        
+        for (int j = 1; j < N; j++) {
             if (arr[j-1] > arr[j])
                 swap(arr, j-1, j);          
         }
-    }
-    PRINT(arr);
+        PRINT(arr);
+    }    
 }
-void InsertionSort(double arr[]) {
-    double target;
+void InsertionSort(int arr[]) {
+    int target;
     int j;
     
     for (int i = 1; i < N; i++) {
@@ -47,11 +55,12 @@ void InsertionSort(double arr[]) {
         for (j = i-1; j >= 0 && arr[j] > target; j--)
             arr[j+1] = arr[j];
         arr[j+1] = target;
-    }
-    PRINT(arr);
+
+        PRINT(arr);
+    }    
 }
 
-void Merge(double arr[], int left, int mid, int right) {
+void Merge(int arr[], int left, int mid, int right) {
     int l = left;
     int r = mid+1;
     int index = left;
@@ -85,8 +94,10 @@ void Merge(double arr[], int left, int mid, int right) {
 
     for (int i = left; i <= right; i++)
         arr[i] = mergeSort[i];
+
+    PRINT(arr);
 }
-void MergeSort(double arr[], int left, int right) {
+void MergeSort(int arr[], int left, int right) {
     if (left == right) return;
 
     int mid = (left+right)/2;
@@ -97,14 +108,16 @@ void MergeSort(double arr[], int left, int right) {
     Merge(arr, left, mid, right);
 }
 
-void QuickSort(double arr[], int start, int end) {
+void QuickSort(int arr[], int start, int end) {
+    PRINT(arr);
+
     if (start >= end) return;
 
     int pivot = start;
 
     int left = start+1;
     int right = end;
-    double tmp;
+    int tmp;
 
     while (left <= right) {
         while (arr[left] <= arr[pivot]) left++;
@@ -116,7 +129,7 @@ void QuickSort(double arr[], int start, int end) {
     QuickSort(arr, start, right-1);
     QuickSort(arr, right+1, end);
 }
-void HeapSort(double arr[]) {
+void HeapSort(int arr[]) {
     for (int i = 1; i < N; i++) {
         int target = i;
         do {
@@ -140,24 +153,48 @@ void HeapSort(double arr[]) {
                 swap(arr, root, target);
             root = target;
         } while (target < i);
-    }
-    PRINT(arr);
+
+        PRINT(arr);
+    }    
 }
 
 int main(void) {
-    for (int i = 0; i < N; i++)
-        cin >> arr[i];
+    printf(">>>>> Array <<<<<\n");
+    PRINT(arr_SEL);
+    printf("\n");
     
-    printf("-----------------------------------------\n");
-    
+    printf(">>>>> Selection Sort <<<<<\n");    
+    SelectionSort(arr_SEL);
+    printf("\n");
+
+    printf(">>>>> Bubble Sort <<<<<\n");    
+    BubbleSort(arr_BUB);
+    printf("\n");
+
+    printf(">>>>> Insertion Sort <<<<<\n");    
+    InsertionSort(arr_INS);
+    printf("\n");
+
+    printf(">>>>> Merge Sort <<<<<\n");    
+    MergeSort(arr_MER, 0, N-1);
+    printf("\n");
+
+    printf(">>>>> Quick Sort <<<<<\n");    
+    QuickSort(arr_QUI, 0, N-1);
+    printf("\n");
+
+    printf(">>>>> Heap Sort <<<<<\n");    
+    HeapSort(arr_HEA);
+    printf("\n");
+
     //SelectionSort(arr);
-    //BubbleSort(arr);
+    
     //InsertionSort(arr);
     /*MergeSort(arr, 0, N-1);
     PRINT(arr);*/
     /*QuickSort(arr, 0, N-1);
     PRINT(arr);*/
-    HeapSort(arr);
+    //HeapSort(arr);
 
     return 0;
 }
