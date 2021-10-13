4b853b097893f3f7722fa080c3404d76c1ce99de
jHoon0223 <aleejaehun@naver.com>
2021-10-14 00:56:11 +0900

    211014 : HW4 Optimal Binary Search Tree

---
+++ HW4/HW4_3.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 7
+
+typedef struct NODE {
+    struct NODE* left;
+    struct NODE* right;
+    char key[10];
+} node;
+
+double A[N][N];
+double R[N][N];
+const double P[7] = { 0, 0.05, 0.15, 0.05, 0.35, 0.05, 0.35 };
+const char KEY[6][10] = { {"CASE"},{"ELSE"},{"END"},{"IF"},{"OF"},{"THEN"} };
+
+double SUM(int begin, int end) {
+    double total = 0;
+    for (int i = begin; i <= end; i++) total += P[i];
+    return total;
+}
+void OBST(int n) {
+    double min;
+
+    for (int i = 1; i <= n; i++) {
+        A[i][i-1] = 0;
+        A[i][i] = P[i];
+        R[i][i] = i;
+        R[i][i-1] = 0;
+    }
+    A[n+1][n] = 0;
+    R[n+1][n] = 0;
+
+    for (int dig = 1; dig <= n-1; dig++) {
+        for (int i = 1; i <= n-dig; i++) {
+            int j = i + dig;
+            min = A[i][i-1] + A[i+1][j] + SUM(i,j);
+
+            for (int k = i; k <= j; k++) {
+                if (min >= A[i][k-1] + A[k+1][j] + SUM(i,j)) {
+                    min = A[i][k-1] + A[k+1][j] + SUM(i, j);
+                    R[i][j] = k;
+                }
+            }
+            A[i][j] = min;
+        }
+    }
+}
+node* TREE(int i, int j) {
+    int k = R[i][j];
+    if (k == 0) return NULL;
+    else {
+        node* p = (node*)malloc(sizeof(node));
+        strcpy(p->key, KEY[k-1]);
+        p->left = TREE(i, k-1);
+        p->right = TREE(k+1, j);
+        return p;
+    }
+}
+void PRINT(double arr[N][N], char c) {
+    for (int i = 1; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            printf("%c[%d][%d] : %.2f\n", c, i, j, arr[i][j]);
+        }
+    }
+    printf("-------------------------\n");
+}
+void PRINT_TREE(node* head) {
+    if (head == NULL) return;
+    else {
+        printf("[%s] ", head->key);
+        PRINT_TREE(head->left);
+        PRINT_TREE(head->right);
+        return;
+    }
+}
+
+int main() {
+    OBST(N-1);
+    PRINT(A, 'A');
+    PRINT(R, 'R');
+
+    node* Tree = TREE(1,6);
+    PRINT_TREE(Tree);
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,71 +1,85 @@
 #include <stdio.h>
-
-#define N 8
-#define INF 99999
-const int W[N][N] = { {0,0,0,0,0,0,0,0},
-                {0,0,4,INF,INF,INF,10,INF,},
-                {0,3,0,INF,18,INF,INF,INF},
-                {0,INF,6,0,INF,INF,INF,INF},
-                {0,INF,5,15,0,2,19,5},
-                {0,INF,INF,12,1,0,INF,INF},
-                {0,INF,INF,INF,INF,INF,0,10},
-                {0,INF,INF,INF,8,INF,INF,0} };
-int P[N][N];
-int D[N][N];
-
-void Floyd(int n, const int W[N][N], int D[N][N], int P[N][N]) {
-    int i, j, k;
-
-    for (i = 1; i < n; i++) {
-        for (j = 1; j < n; j++) {
-            P[i][j] = 0;
-        }
-    }
-
-    for (i = 1; i < N; i++) {
-        for (j = 1; j < N; j++) {
-            D[i][j] = W[i][j];
-        }
-    }
-
-    for (k = 1; k < n; k++) {
-        for (i = 1; i < n; i++) {
-            for (j = 1; j < n; j++) {
-                if (D[i][k] + D[k][j] < D[i][j]) {
-                    P[i][j] = k;
-                    D[i][j] = D[i][k] + D[k][j];
-                }
-            }
-        }
-    }
+//key1,key2...keyn
+typedef int index;//
+const float p[7] = { NULL, 0.05, 0.15, 0.05, 0.35, 0.5, 0.35};
+float sum(index begin, index end)
+{
+float sum_total = 0;
+for (int i = begin; i <= end; i++)
+{
+sum_total += p[i];
 }
-void Path(int a, int b) {
-    printf("path(%d,%d) = %d\n", a, b, P[a][b]);
-
-    if (P[a][b] != 0) {
-        Path(a, P[a][b]);
-        printf("v%d \n", P[a][b]);
-        Path(P[a][b], b);
-    }
+return sum_total;
 }
-void PRINT(const int arr[N][N]) {
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
-            printf("%d ", arr[i][j]);
-        }
-        printf("\n");
-    }
-    printf("-------------------------\n");
+//optimal search tree
+void optsearchtree(index n,index **R)
+{
+index i, j, k, diagonal;
+float **A = new float*[n + 2];
+float min;
+for (int s = 0; s < n + 2; s++)
+A[s] = new float[n + 1];
+// 계산 안하는 구간 처리
+for (i = 1; i <= n; i++)
+{
+A[i][i - 1] = 0;
+A[i][i] = p[i];
+A[0][i - 1] = NULL;
+R[i][i] = i;
+R[i][i - 1] = 0;
+R[0][i - 1] = NULL;
 }
-
+A[0][n] = NULL;
+A[n + 1][n] = 0;
+R[0][n] = NULL;
+R[n + 1][n] = 0;
+for (diagonal = 1; diagonal <= n-1; diagonal++)
+{
+for (i = 1; i <= n - diagonal; i++)
+{
+j = i + diagonal;
+min = A[i][i-1] + A[i + 1][j] + sum(i,j);
+for (k = i; k <= j; k++)
+{
+if (min >= A[i][k - 1] + A[k + 1][j] + sum(i, j))
+{
+min = A[i][k-1] + A[k + 1][j] + sum(i, j);
+R[i][j] = k;
+}
+}
+A[i][j] = min;
+}
+}
+for (index i = 1; i <= n; i++)
+{
+for (index j = i; j <= n; j++)
+{
+printf("A[%d][%d] ::%.2f", i, j, A[i][j]);
+printf("\n");
+}
+}
+for (int s = 0; s < n + 2; s++)
+delete[]A[s];
+}
 int main() {
-    Floyd(N, W, D, P);
-    
-    PRINT(W);
-    PRINT(D);
-    PRINT(P);
-
-    Path(1, 3);
-
-    return 0;
+int n = sizeof(p) / sizeof(float)-1;
+//R[i][j] i~j
+index **R=new index *[n + 2];
+for (int i = 0; i < n + 2; i++)
+{
+R[i] = new index[n + 1];
+}
+optsearchtree(n,R);
+for (int i = 1; i <= n; i++)
+{
+for (int j = i; j <= n; j++)
+{
+printf("R[%d][%d] ::%d", i, j, R[i]
+[j]);
+printf("\n");
+}
+}
+for (int i = 0; i < n + 2; i++)
+delete[] R[i];
+return 0;
 }
