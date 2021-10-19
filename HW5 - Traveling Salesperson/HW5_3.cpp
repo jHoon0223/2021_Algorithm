849133cd0195d3c73f7f441b65cdaadc3d0624b7
jHoon0223 <aleejaehun@naver.com>
2021-10-20 02:54:42 +0900

    211020 : HW5 - modified Traveling Salesperson Algorithm

--- HW5 - Traveling Salesperson/HW5_3.cpp
+++ HW5 - Traveling Salesperson/HW5_3.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-#define N 4
+#define N 7
 #define INF 9999
 
-int W[N][N] = { {0,2,9,INF},
-                {1,0,6,4},
-                {INF,7,0,8},
-                {6,3,INF,0} };
+int W[N][N] = { {0,4,INF,INF,INF,10,INF,},
+                {3,0,INF,18,INF,INF,INF},
+                {INF,6,0,INF,INF,INF,INF},
+                {INF,5,15,0,2,19,5},
+                {INF,INF,12,1,0,INF,INF},
+                {INF,INF,INF,INF,INF,0,10},
+                {INF,INF,INF,8,INF,INF,0} };
 
 int TSP(int W[N][N], int T[N], int start, int n) {
     int tmp[N], minT[N];
